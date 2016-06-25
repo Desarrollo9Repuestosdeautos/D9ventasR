9bde225207fcbfc51f0c6d0af483affd9d086ee2
Javier <javiergutierrrez98@gmail.com>
2016-06-25 02:31:30 -0500

    datos nuevos
    base de datos se agregaron campos nuevos

--- proyecto.sql
+++ proyecto.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: localhost
--- Tiempo de generación: 23-06-2016 a las 02:53:06
+-- Tiempo de generación: 25-06-2016 a las 09:30:29
 -- Versión del servidor: 10.1.13-MariaDB
 -- Versión de PHP: 5.6.20
 
@@ -23,17 +23,34 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `inventario`
+-- Estructura de tabla para la tabla `categoria`
 --
 
-CREATE TABLE `inventario` (
-  `precio` int(11) NOT NULL,
-  `nombre_articulo` varchar(20) CHARACTER SET utf8 NOT NULL,
-  `codigo_producto` varchar(20) CHARACTER SET utf8 NOT NULL,
-  `vendedor` varchar(20) CHARACTER SET utf8 NOT NULL,
-  `descripcion` varchar(500) CHARACTER SET utf8 NOT NULL,
-  `envio` varchar(20) CHARACTER SET utf8 NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf32;
+CREATE TABLE `categoria` (
+  `id` int(11) NOT NULL,
+  `categoria` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `categoria`
+--
+
+INSERT INTO `categoria` (`id`, `categoria`) VALUES
+(1, 'Compatible con la inspeccion de automoviles'),
+(2, 'T/M×Tren de engranajes'),
+(3, 'Repuestos con referencia de Direccion'),
+(4, 'Acondicionador de aire y calefaccion'),
+(5, 'Accesorio interior'),
+(6, 'Extremo delantero'),
+(7, 'Parte trasera final'),
+(8, 'Repuestos con referencia de Motor'),
+(9, 'Repuestos con referencia de Suspension'),
+(10, 'Repuestos con referencia de Frenos'),
+(11, 'Audio'),
+(12, 'Componente electrico'),
+(13, 'Audio'),
+(14, 'Parte lateral'),
+(15, 'Parte de vidrio');
 
 -- --------------------------------------------------------
 
@@ -43,31 +60,35 @@ CREATE TABLE `inventario` (
 
 CREATE TABLE `inventario2` (
   `ID` int(11) NOT NULL,
-  `precio` int(100) NOT NULL,
+  `precio` float NOT NULL,
   `vendedor1` varchar(30) NOT NULL,
-  `codigo` varchar(30) NOT NULL,
   `nombre_producto` varchar(30) NOT NULL,
   `imagen1` varchar(30) NOT NULL,
   `descripcion` varchar(200) NOT NULL,
-  `envio` int(20) NOT NULL
+  `categoria` varchar(100) NOT NULL,
+  `year` varchar(100) NOT NULL,
+  `modelo` varchar(200) NOT NULL,
+  `marca` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Volcado de datos para la tabla `inventario2`
 --
 
-INSERT INTO `inventario2` (`ID`, `precio`, `vendedor1`, `codigo`, `nombre_producto`, `imagen1`, `descripcion`, `envio`) VALUES
-(3, 34, 'alvin', 'qwe', 'mofle', 'imagenes/imagenesmofle.jpg', 'nuevo', 2),
-(7, 23, 'dfdf', 'dfdf', 'dfdf', 'imagenes/imagenesimac.jpg', 'dada', 2),
-(10, 456, 'juan', 'rr', 'mofle', 'imagenes/imagenesmolfe2.jpg', 'nuevo de fabrica', 4),
-(11, 45, 'dfdfd', 'oudfed', 'ohfipadf', 'imagenes/imagenesejemplo.png', 'Ã±ouhfushfs', 1),
-(14, 8989, 'luis', 'rtry', 'mofle', 'imagenes/imagenesmofle.jpg', 'nuevo ', 5),
-(15, 180, 'juan', '1412', 'radiador', 'imagenes/imagenesradiador.jpeg', 'esta excelente 2 aÃ±os de uso', 65),
-(16, 67, 'jh', '76', 'jkhk', 'imagenes/imagenesIMG_3687.JPG', 'hkjhk', 786);
+INSERT INTO `inventario2` (`ID`, `precio`, `vendedor1`, `nombre_producto`, `imagen1`, `descripcion`, `categoria`, `year`, `modelo`, `marca`) VALUES
+(1, 185, 'absdjab', 'motor', 'imagenes/imagenesmotor.jpg', 'bhjbhbj', '8', '879', 'nknk', 'njkn'),
+(2, 182.98, 'jhvgv', 'halo', 'imagenes/imagenesHalo.jpg', 'mbhjbjhb', '1', '8768', 'jhbhjbjbj', 'bhjjbhj'),
+(3, 878.435, 'zol', 'lk', 'imagenes/imagenesmotor.jpg', 'jbhbhhb', '3', '8979879', 'knknkn', 'jkjjkn');
 
 --
 -- Índices para tablas volcadas
 --
+
+--
+-- Indices de la tabla `categoria`
+--
+ALTER TABLE `categoria`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indices de la tabla `inventario2`
@@ -80,10 +101,15 @@ ALTER TABLE `inventario2`
 --
 
 --
+-- AUTO_INCREMENT de la tabla `categoria`
+--
+ALTER TABLE `categoria`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+--
 -- AUTO_INCREMENT de la tabla `inventario2`
 --
 ALTER TABLE `inventario2`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
